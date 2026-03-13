12b38bd507dbd6a80e998822b169d6ffbeadac00
mosca <janela.mosca@walldorfconsulting.com>
2026-03-13 21:38:00 +0800

    fixes for test

--- app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -1,9 +1,24 @@
 module.exports = {
-  /**
-    * define actions for the page object
-  */
+    iClickOnCreateNewBook: async () => {
+        await browser.asControl({
+            selector: {
+                id: "books::BooksList--fe::table::Books::LineItem::StandardAction::Create",
+                interaction: {
+                    idSuffix: "BDI-content"
+                }
+            }
+        }).press();
+    },
 
-  /**
-   * define assertions for the page object
-   */
+    theBookListContains: async (sBook) => {
+        const text = await browser.asControl({
+            selector: {
+                controlType: "sap.m.Text",
+                viewId: "books::BooksList",
+                properties: {
+                        text: sBook
+                }
+        }}).getText();
+        expect(text).toEqual(sBook);
+    }
 }
--- app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
@@ -1,9 +1,83 @@
 module.exports = {
-        /**
-         * define actions for the page object
-         */
+    iEnterTitle: async (sBook) => {
+        await browser.asControl({
+            selector: {
+                id: "books::BooksDetailsList--fe::EditableHeaderForm::EditableHeaderTitle::Field-edit",
+                interaction: {
+                    idSuffix: "inner",
+                },
+            },
+        }).enterText(sBook);
+    },
 
-        /**
-         * define assertions for the page object
-         */
+    iSelectGenre: async () => {
+        await browser.asControl({
+            selector: {
+                id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::genre_ID::Field-edit-inner-vhi"
+            }
+        }).press();
+
+        await browser.asControl({
+            selector: {
+                controlType: "sap.fe.core.controls.FieldWrapper",
+                viewId: "books::BooksDetailsList",
+                searchOpenDialogs: true,
+                descendant: {
+                        controlType: "sap.m.Text",
+                        viewId: "books::BooksDetailsList",
+                        bindingPath: {
+                                path: "/Genres(10)",
+                                propertyPath: "ID"
+                        },
+                        searchOpenDialogs: true
+                }
+            }
+        }).press();
+    },
+
+    iSelectAuthor: async () => {
+        await browser.asControl({
+            selector: {
+                id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::author_ID::Field-edit-inner-vhi"
+            }
+        }).press();
+
+        await browser.asControl({
+            selector: {
+                controlType: "sap.m.Text",
+                viewId: "books::BooksDetailsList",
+                bindingPath: {
+                        path: "/Authors(101)",
+                        propertyPath: "name"
+                },
+                searchOpenDialogs: true
+            }
+        }).press();
+    },
+
+    iPressCreate: async () => {
+        await browser.asControl({
+            selector: {
+                id: "books::BooksDetailsList--fe::FooterBar::StandardAction::Save",
+                interaction: {
+                        idSuffix: "content"
+                }
+            }
+        }).press();
+    },
+
+    iSeeEditButton: async () => {
+        const enabled = await browser.asControl({
+            selector: {
+                id: "books::BooksDetailsList--fe::StandardAction::Edit"
+        }}).getEnabled();
+        expect(enabled).toBeTruthy();
+    },
+
+    iNavigateBack: async () => {
+        await browser.asControl({
+            selector: {
+                id: "backBtn"
+        }}).press();
+    }
 };
